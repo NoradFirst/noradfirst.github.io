2b31b6ca43c597489926ad13b0e816e4a6e872d5
NoradFirst <masterjf@hotmail.com>
2016-03-17 23:44:51 +0100

    none?

--- js/beer.js
+++ js/beer.js
@@ -1,12 +1,11 @@
-(function() {
-    var app = angular.module('beerStore', []);
+(function (){
+    var app=angular.module('beerStore',[]);
 
-    app.controller('StoreController', ['$http', function($http){
+    app.controller('StoreController',['$http',function($http){
+    //app.controller('StoreController',function(){
+    // this.beers=test;});
         var store = this;
-        store.beers = [];
-        $http.get('/beer.json').success(function(data){
-            store.beers=data;
-        });
+        store.beers=[];
+        $http.get('/beer.json').success(function(data){store.beers=data;})
     }]);
-
 })();
