db54041314869f0acf4b2a96c2c2dbd03d1730a8
NoradFirst <masterjf@hotmail.com>
2016-03-20 20:49:59 +0100

    js/beer.json

--- js/beer.js
+++ js/beer.js
@@ -1,11 +1,8 @@
-(function (){
     var app=angular.module('beerStore',[]);
 
     app.controller('StoreController',['$http',function($http){
-    //app.controller('StoreController',function(){
-    // this.beers=test;});
         var store = this;
         store.beers=[];
         $http.get('js/beer.json').success(function(data){store.beers=data;})
     }]);
-})();
+
