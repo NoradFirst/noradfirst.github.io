3793e1a440c2601f9d1f31e994d08a3d1ada9fa1
NoradFirst <masterjf@hotmail.com>
2016-03-22 23:32:47 +0100

    sorting and filtering

--- js/beer.js
+++ js/beer.js
@@ -1,8 +1,14 @@
     var app=angular.module('beerStore',[]);
 
-    app.controller('StoreController',['$http',function($http){
+    app.controller('StoreController',['$http','$scope',function($http,$scope){
         var store = this;
         store.beers=[];
-        $http.get('js/beer.json').success(function(data){store.beers=data;})
+        $http.get('js/beer.json').success(function(data){store.beers=data;});
+
+        $scope.sortType     = 'Nom'; // set the default sort type
+        $scope.sortReverse  = false;  // set the default sort order
+        $scope.searchBeer   = '';     // set the default search/filter term
+
+        $scope.beers = store.beers;
     }]);
 
