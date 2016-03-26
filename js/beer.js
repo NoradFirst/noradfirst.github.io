0aed694cbe8f30b064b7238d27d3115ef72064de
NoradFirst <masterjf@hotmail.com>
2016-03-26 20:33:06 +0100

    Color filter

--- js/beer.js
+++ js/beer.js
@@ -8,6 +8,38 @@
         $scope.sortType     = 'Nom'; // set the default sort type
         $scope.sortReverse  = false;  // set the default sort order
         $scope.searchBeer   = '';     // set the default search/filter term
+        $scope.color = 'all';
+        $scope.imgClassAll="over";
+
+        $scope.changeClass = function(){
+            $scope.imgClassAll="transp";
+            $scope.imgClassBlanche="transp";
+            $scope.imgClassBlonde="transp";
+            $scope.imgClassAmbree="transp";
+            $scope.imgClassRubis="transp";
+            $scope.imgClassBrune="transp";
+            $scope.imgClassNoire="transp";
+            if ($scope.color == 'Blanche')
+                $scope.imgClassBlanche="over";
+            if ($scope.color == 'Blonde')
+                $scope.imgClassBlonde="over";
+            if ($scope.color != 'Blanche' && $scope.color != 'Blonde' && $scope.color != 'Rubis' && $scope.color != 'Brune' && $scope.color != 'Noire'&& $scope.color != 'all')
+                $scope.imgClassAmbree="over";
+            if ($scope.color == 'Rubis')
+                $scope.imgClassRubis="over";
+            if ($scope.color == 'Brune')
+                $scope.imgClassBrune="over";
+            if ($scope.color == 'Noire')
+                $scope.imgClassNoire="over";
+            if ($scope.color == 'all'){
+                $scope.imgClassAll="over";
+                $scope.imgClassBlanche="";
+                $scope.imgClassBlonde="";
+                $scope.imgClassAmbree="";
+                $scope.imgClassRubis="";
+                $scope.imgClassBrune="";
+                $scope.imgClassNoire="";}
+        };
 
         $scope.beers = store.beers;
     }]);
